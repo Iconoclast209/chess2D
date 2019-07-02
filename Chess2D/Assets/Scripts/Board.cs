46b2c2e249b90f2e0ffe853e005d8a776c394cc1
Iconoclast209 <mrpresidentsmith@gmail.com>
2019-07-01 17:01:49 -0700

    Created project and Board, Cell, and GameManager classes.

---
+++ Chess2D/Assets/Scripts/Board.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Board : MonoBehaviour
+{
+    #region FIELDS
+    public GameObject mCellPrefab;
+    public Cell[,] mAllCells = new Cell[8, 8];
+    #endregion
+
+    #region METHODS
+    public void Create()
+    {
+        for (int y = 0; y < 8; y++)
+        {
+            for(int x = 0;x < 8;x++)
+            {
+                //Create the Cell
+                GameObject newCell = Instantiate(mCellPrefab, transform);
+                //Position the Cell
+                RectTransform rectTransform = newCell.GetComponent<RectTransform>();
+                rectTransform.anchoredPosition = new Vector2((x*100) + 50,(y*100) + 50);
+                //Setup the Cell
+                mAllCells[x, y] = newCell.GetComponent<Cell>();
+                mAllCells[x, y].Setup(new Vector2Int(x, y), this);
+                Debug.Log("Created new cell at " + x + ", " + y);
+
+            }
+        }
+
+        for(int x = 0; x < 8; x += 2)
+        {
+            for(int y = 0; y < 8; y++)
+            {
+                int offset = (y % 2 != 0) ? 0 : 1;
+                int finalX = x + offset;
+                mAllCells[finalX, y].GetComponent<Image>().color = new Color32(230, 220, 187, 255);
+            }
+        }
+    }
+    #endregion
+}
---
+++ Chess2D/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Board mBoard;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Creating Board");
+        mBoard.Create();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
