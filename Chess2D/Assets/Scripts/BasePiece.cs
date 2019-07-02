a5e915d5d63f7262f041e2a197eba5af008e10f9
Iconoclast209 <mrpresidentsmith@gmail.com>
2019-07-02 06:53:41 -0700

    Created BasePiece class and empty classes for each piece type.

---
+++ Chess2D/Assets/Scripts/BasePiece.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public abstract class BasePiece : EventTrigger
+{
+    #region FIELDS
+    public Color mColor = Color.clear;
+    protected Cell mOriginalCell = null;
+    protected Cell mCurrentCell = null;
+    protected RectTransform mRectTransform = null;
+    protected PieceManager mPieceManager;
+    #endregion
+
+
+    #region METHODS
+    public virtual void Setup(Color newTeamColor, Color32 newSpriteColor, PieceManager newPieceManager)
+    {
+        mPieceManager = newPieceManager;
+        mColor = newTeamColor;
+        GetComponent<Image>().color = newSpriteColor;
+        mRectTransform = GetComponent<RectTransform>();
+    }
+
+    public void Place(Cell newCell)
+    {
+        //Cell
+        mCurrentCell = newCell;
+        mOriginalCell = newCell;
+        mCurrentCell.mCurrentPiece = this;
+
+        //Object
+        transform.position = newCell.transform.position;
+        gameObject.SetActive(true);
+    }
+
+    #endregion
+}
