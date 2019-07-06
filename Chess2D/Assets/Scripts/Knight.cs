f118bf684a71d9a74aa956a0cb6d2f8f120db271
Iconoclast209 <mrpresidentsmith@gmail.com>
2019-07-06 06:38:05 -0700

    Video 6.  Added movement functionality for all pieces except for Knight.

--- Chess2D/Assets/Scripts/Knight.cs
+++ Chess2D/Assets/Scripts/Knight.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Knight : BasePiece
 {
-    
+    public override void Setup(Color newTeamColor, Color32 newSpriteColor, PieceManager newPieceManager)
+    {
+        base.Setup(newTeamColor, newSpriteColor, newPieceManager);
+
+        //mMovement = new Vector3Int(7, 7, 0);
+        GetComponent<Image>().sprite = Resources.Load<Sprite>("T_Knight");
+    }
 }
--- Chess2D/Assets/Scripts/Pawn.cs
+++ Chess2D/Assets/Scripts/Pawn.cs
@@ -1,8 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pawn : BasePiece
 {
-   
+    private bool mIsFirstMove = true;
+
+    public override void Setup(Color newTeamColor, Color32 newSpriteColor, PieceManager newPieceManager)
+    {
+        base.Setup(newTeamColor, newSpriteColor, newPieceManager);
+        mIsFirstMove = true;
+        if(mColor == Color.white)
+        {
+            mMovement = new Vector3Int(0, 1, 1);
+        }
+        else
+        {
+            mMovement = new Vector3Int(0, -1, -1);
+        }
+        GetComponent<Image>().sprite = Resources.Load<Sprite>("T_Pawn");
+    }
+
+    protected override void Move()
+    {
+        base.Move();
+        mIsFirstMove = false;
+    }
+
+    private bool MatchesState(int targetX, int targetY, CellState targetState)
+    {
+        CellState cellState = CellState.None;
+        cellState = mCurrentCell.mBoard.ValidateCell(targetX, targetY, this);
+
+        if(cellState == targetState)
+        {
+            mHighlightedCells.Add(mCurrentCell.mBoard.mAllCells[targetX, targetY]);
+            return true;
+        }
+        return false;
+    }
+
+    protected override void CheckPathing()
+    {
+        int currentX = mCurrentCell.mBoardPosition.x;
+        int currentY = mCurrentCell.mBoardPosition.y;
+
+        MatchesState(currentX - mMovement.z, currentY + mMovement.z, CellState.Enemy);
+        MatchesState(currentX + mMovement.z, currentY - mMovement.z, CellState.Enemy);
+        if(MatchesState(currentX, currentY + mMovement.y, CellState.Free))
+        {
+            if(mIsFirstMove)
+            {
+                MatchesState(currentX, currentY + (mMovement.y * 2), CellState.Free);
+            }
+        }
+    }
 }
